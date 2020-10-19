f2f3b7ba8514455a1c53ad42ea517782409ac52e
Rita <rita@ritacorreia.co>
2020-10-19 16:15:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,54 @@ const ctx = canvas.getContext('2d'); // asks for 2d context
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight; // sets canvas as big as the width and height of the window
 
-ctx.strokeStyle = '#BADA55'
+ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+ctx.globalCompositeOperation = 'lighten';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(event) {
+    if (!isDrawing) return; // stops the function from running when they are not moused down
+    console.log(event);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    //start from
+    ctx.moveTo(lastX, lastY);
+    // go to
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+    if (ctx.lineWidth >= 70 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+
+
+}
+
+canvas.addEventListener('mousedown', (event) => {
+    isDrawing = true;
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+});
+canvas.addEventListener('mousemove', draw);
+
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
